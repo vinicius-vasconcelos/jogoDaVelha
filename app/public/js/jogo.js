e3fe5c5a30589495fc244fad01ba1997d6ecfd8e
vinicius <viniciussouzav@gmail.com>
2019-05-11 02:05:47 -0300

    finalizado as funçoes de verificação

--- app/public/js/jogo.js
+++ app/public/js/jogo.js
@@ -1,9 +1,13 @@
+/* Matriz globar das posições 
+    (inicializei assim para não dar bosta no meu if la em baixo)
+*/
 mat = [
     ['00', '01', '02'],
     ['10', '11', '12'],
     ['20', '21', '22']
 ];
 
+//função de limpar o texto das divs e voltar a matriz ao normal 
 function limpaTela() {
     mat = [
         ['00', '01', '02'],
@@ -11,13 +15,15 @@ function limpaTela() {
         ['20', '21', '22']
     ];
 
-    $('.col').each(function(){
+    $('.col').each(function() { //percorrendo todas minhas divs
         this.innerHTML = '';
     });
 }
 
 //função pata esperar o documento html carregar
 $(document).ready(() => {
+
+    //função para verificar todas as possibilidades de resultados
     function verificaVencedor() {
         if( (mat[0][0] == mat[0][1] && mat[0][1] == mat[0][2]) ||
             (mat[0][0] == mat[1][0] && mat[1][0] == mat[2][0]) ||
@@ -30,24 +36,24 @@ $(document).ready(() => {
             }
     }
 
-
+    //função de click na div que deseja ser inserido um valor
     $('.col').click(function() {
         let valor = prompt('1 ou 0');
 
+        //se o usuário digitou algum valor inválido
         if(valor != '1' && valor != '0') {
             alert('Digite apenas 1 ou 0');
             return;
-        } else {
+        } else { //se não ele atualiza a mat(global) e coloca um texto na div
             let str = this.id.split('#');
             let lin = parseInt(str[0]);
             let col = parseInt(str[1]);
 
-           // console.log(`lin = ${lin}; col = ${col} == mat${mat[lin][col]}`)
             if(mat[lin][col] != '0' && mat[lin][col] != '1') {
                 mat[lin][col] = valor;
                 document.getElementById(this.id).innerHTML = valor;
                 
-                verificaVencedor();
+                verificaVencedor(); // a cada novo click verificar se tem um vencedor 
             }
         }
     });
