4b9f9b09af2db2696be0d0661a53ba3b77bd67e0
vinicius <viniciussouzav@gmail.com>
2019-06-22 20:07:30 -0300

    Atualizaçao de bug e modo de jogar

--- app/public/js/jogo.js
+++ app/public/js/jogo.js
@@ -1,11 +1,13 @@
 /* Matriz globar das posições 
     (inicializei assim para não dar bosta no meu if la em baixo)
 */
-mat = [
+var mat = [
     ['00', '01', '02'],
     ['10', '11', '12'],
     ['20', '21', '22']
 ];
+
+var x = true;
 
 //função de limpar o texto das divs e voltar a matriz ao normal 
 function limpaTela() {
@@ -27,35 +29,38 @@ $(document).ready(() => {
     function verificaVencedor() {
         if( (mat[0][0] == mat[0][1] && mat[0][1] == mat[0][2]) ||
             (mat[0][0] == mat[1][0] && mat[1][0] == mat[2][0]) ||
+            (mat[0][2] == mat[1][1] && mat[1][1] == mat[2][0]) ||
             (mat[2][0] == mat[2][1] && mat[2][1] == mat[2][2]) ||
             (mat[0][2] == mat[1][2] && mat[1][2] == mat[2][2]) ||
             (mat[0][0] == mat[1][1] && mat[1][1] == mat[2][2]) ||
             (mat[1][0] == mat[1][1] && mat[1][1] == mat[1][2])) {
                 alert('Ganhou');
-                limpaTela();
+                setTimeout(() =>  limpaTela(), 1000);
             }
     }
 
     //função de click na div que deseja ser inserido um valor
     $('.col').click(function() {
-        let valor = prompt('1 ou 0');
 
-        //se o usuário digitou algum valor inválido
-        if(valor != '1' && valor != '0') {
-            alert('Digite apenas 1 ou 0');
-            return;
-        } else { //se não ele atualiza a mat(global) e coloca um texto na div
-            let str = this.id.split('#');
-            let lin = parseInt(str[0]);
-            let col = parseInt(str[1]);
+        let str = this.id.split('#');
+        let lin = parseInt(str[0]);
+        let col = parseInt(str[1]);
 
-            //verficando se tem lugar vago na matriz
-            if(mat[lin][col] != '0' && mat[lin][col] != '1') {
-                mat[lin][col] = valor;
-                document.getElementById(this.id).innerHTML = valor;
-                
-                verificaVencedor(); // a cada novo click verificar se tem um vencedor 
+        //verficando se tem lugar vago na matriz
+        if(mat[lin][col] != '0' && mat[lin][col] != '1') {
+            if(x) {
+                mat[lin][col] = 1;
+                x = false;
+                document.getElementById(this.id).innerHTML = 1;
             }
+            else {
+                mat[lin][col] = 0;
+                x = true;
+                document.getElementById(this.id).innerHTML = 0;
+            }
+            
+            verificaVencedor(); // a cada novo click verificar se tem um vencedor 
         }
+    
     });
 })
